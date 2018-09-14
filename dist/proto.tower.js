45581fc9abf72d86bf2f4e9e1764d39818f6ae83
mihailo-misic <mihailo.misic@gmail.com>
2018-09-14 12:22:29 +0200

    UPDATE: Increased the amount to keep in walls and ramparts

--- dist/proto.tower.js
+++ dist/proto.tower.js
@@ -5,7 +5,7 @@ StructureTower.prototype.defend = function () {
   } else {
     // Look for broken walls first.
     let target = this.pos.findClosestByRange(FIND_STRUCTURES,
-        { filter: (s) => (s.hits <= 5000) && s.structureType === STRUCTURE_WALL },
+        { filter: (s) => (s.hits <= 20000) && (s.structureType === STRUCTURE_WALL || s.structureType === STRUCTURE_RAMPART) },
     );
     // Look for other stuff if walls are fine.
     if (!target) {
